32a84b40ae1f3cccb5acc8028f7d4a4375d1766d
Ishmael-sikhikhi <i.sikhikhi@gmail.com>
2021-11-02 12:26:49 +0200

    untracked node_modules

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
-'use strick'
+'use strick';
 
-let express = require('express');
+const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
 
+const pg = require('pg');
 const Waiter = require('./routes/waiters');
 
 const WaitersService = require('./services/waiters-service');
 
-const pg = require("pg");
-const Pool = pg.Pool;
-let app = express();
+const { Pool } = pg;
+const app = express();
 
-// should we use a SSL connection
-let useSSL = false;
-let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local) {
-    useSSL = true;
-}
 // which db connection to use
 const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/waiters';
 
 const pool = new Pool({
-    connectionString,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const waitersService = WaitersService(pool);
 
 const waiterRoute = Waiter(waitersService);
 
-const { request } = require('express');
-const flash = require('express-flash');
-const session = require('express-session');
-
 // initialise session middleware - flash-express depends on it
 app.use(session({
-    secret: 'this is my long String that is used for session in http',
-    resave: false,
-    saveUninitialized: true
+  secret: 'this is my long String that is used for session in http',
+  resave: false,
+  saveUninitialized: true,
 }));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -48,18 +40,19 @@ app.set('view engine', 'handlebars');
 // initialise the flash middleware
 app.use(flash());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/jsongreetInstance
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', waiterRoute.defaultRoute)
-app.get('/waiters/:username', waiterRoute.waitersPage)
-app.post('/waiters/:username', waiterRoute.subscribe)
-app.get('/owner', waiterRoute.waitersAvailable)
-app.get('/reset', waiterRoute.weeklyReset)
-const PORT = process.env.PORT || 3000
+app.get('/', waiterRoute.defaultRoute);
+app.get('/waiters/:username', waiterRoute.waitersPage);
+app.post('/waiters/:username', waiterRoute.subscribe);
+app.get('/owner', waiterRoute.waitersAvailable);
+app.get('/reset', waiterRoute.weeklyReset);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`App started at port:${PORT}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`App started at port:${PORT}`);
+});
--- test/waiters-test.js
+++ test/waiters-test.js
@@ -1,234 +1,233 @@
-
 const assert = require('assert');
+const pg = require('pg');
 const WaiterService = require('../services/waiters-service');
-const pg = require("pg");
-const Pool = pg.Pool;
 
+const { Pool } = pg.Pool;
 
 // we are using a special test database for the tests
 const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/waiter_test';
 
 const pool = new Pool({
-    connectionString
+  connectionString,
 });
 
-let waiters = WaiterService(pool)
+const waiters = WaiterService(pool);
 
-beforeEach(async function () {
-    // clean the tables before each test run
-    await waiters.deleteRecord();
-
+// eslint-disable-next-line no-undef
+beforeEach(async () => {
+// clean the tables before each test run
+  await waiters.deleteRecord();
 });
 
 // eslint-disable-next-line no-undef
 describe('Waiter avalability', () => {
-    it('Should add waiter\' shifts', async () => {
-        await waiters.deleteRecord();
-        //add name
-        var days = [1, 2, 3]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
+  // eslint-disable-next-line no-undef
+  it('Should add waiter\' shifts', async () => {
+    await waiters.deleteRecord();
 
-        assert.deepEqual([{ name: 'Apiwe', day: 'Sunday' }, { name: 'Apiwe', day: 'Monday' }, { name: 'Apiwe', day: 'Tuesday' }], await waiters.getWaiters())
+    const days = [1, 2, 3];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
 
-    })
-    it('Should update waiter\' days if waiter changes shift(s)', async () => {
-        await waiters.deleteRecord();
-        //add name
-        var days = [1, 2, 3]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
-        days = [1, 2, 5]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
-        assert.deepEqual([{ name: 'Apiwe', day: 'Sunday' }, { name: 'Apiwe', day: 'Monday' }, { name: 'Apiwe', day: 'Thursday' }], await waiters.getWaiters())
+    assert.deepEqual([{ name: 'Apiwe', day: 'Sunday' }, { name: 'Apiwe', day: 'Monday' }, { name: 'Apiwe', day: 'Tuesday' }], await waiters.getWaiters());
+  });
+  // eslint-disable-next-line no-undef
+  it('Should update waiter\' days if waiter changes shift(s)', async () => {
+    await waiters.deleteRecord();
+    // add name
+    let days = [1, 2, 3];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
+    days = [1, 2, 5];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
+    assert.deepEqual([{ name: 'Apiwe', day: 'Sunday' }, { name: 'Apiwe', day: 'Monday' }, { name: 'Apiwe', day: 'Thursday' }], await waiters.getWaiters());
+  });
 
-    })
-   
-    it('Shoud return user id', async () => {
-        await waiters.deleteRecord()
-        var days = [1, 2, 3]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
-        await waiters.selectDay({ name: 'Mandla', day: days })
-        await waiters.selectDay({ name: 'Lwazi', day: days })
+  // eslint-disable-next-line no-undef
+  it('Shoud return user id', async () => {
+    await waiters.deleteRecord();
+    const days = [1, 2, 3];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
+    await waiters.selectDay({ name: 'Mandla', day: days });
+    await waiters.selectDay({ name: 'Lwazi', day: days });
 
-        assert.equal(1, await waiters.getUsersID("Apiwe"))
+    assert.equal(1, await waiters.getUsersID('Apiwe'));
+  });
 
-    })
+  // eslint-disable-next-line no-undef
+  it('update function help to update users', async () => {
+    await waiters.deleteRecord();
+    let days = [1, 2, 3];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
+    await waiters.updateShieft(1);
+    days = [1, 4, 5];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
+    assert.deepEqual([
+      {
+        day: 'Sunday',
+        name: 'Apiwe',
+      },
+      {
+        day: 'Wednesday',
+        name: 'Apiwe',
+      },
+      {
+        day: 'Thursday',
+        name: 'Apiwe',
+      },
+    ], await waiters.getWaiters());
+  });
+  // eslint-disable-next-line no-undef
+  it('Should reset able to reset shifts', async () => {
+    await waiters.deleteRecord();
+    assert.deepEqual([], await waiters.getWaiters());
+  });
+  // eslint-disable-next-line no-undef
+  it('Function return days', async () => {
+    await waiters.deleteRecord();
 
-    it('update function help to update users', async ()=>{
-        await waiters.deleteRecord()
-        var days = [1, 2, 3]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
-        await waiters.updateShieft(1)
-        days = [1, 4, 5]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
-        assert.deepEqual([
-            {
-              "day": "Sunday",
-              "name": "Apiwe"
-            },
-            {
-              "day": "Wednesday",
-              "name": "Apiwe"
-            },
-            {
-              "day": "Thursday",
-              "name": "Apiwe"
-            }
-          ], await waiters.getWaiters())
-    })
-    it('Should reset able to reset shifts', async () => {
-        await waiters.deleteRecord();        
-        assert.deepEqual([], await waiters.getWaiters())
-    })
-    it('Function return days', async () => {
-        await waiters.deleteRecord()
+    assert.deepEqual([
+      {
+        day: 'Sunday',
+        id: 1,
+      },
+      {
+        day: 'Monday',
+        id: 2,
+      },
+      {
+        day: 'Tuesday',
+        id: 3,
+      },
+      {
+        day: 'Wednesday',
+        id: 4,
+      },
+      {
+        day: 'Thursday',
+        id: 5,
+      },
+      {
+        day: 'Friday',
+        id: 6,
+      },
+      {
+        day: 'Saturday',
+        id: 7,
+      },
+    ], await waiters.getDays());
+  });
 
-        assert.deepEqual([
-            {
-                day: 'Sunday',
-                id: 1
-            },
-            {
-                day: 'Monday',
-                id: 2
-            },
-            {
-                day: 'Tuesday',
-                id: 3
-            },
-            {
-                day: 'Wednesday',
-                id: 4
-            },
-            {
-                day: 'Thursday',
-                id: 5
-            },
-            {
-                day: 'Friday',
-                id: 6
-            },
-            {
-                day: 'Saturday',
-                id: 7
-            }
-        ], await waiters.getDays())
+  // eslint-disable-next-line no-undef
+  it('Should check if days which days are selected by logged waiter', async () => {
+    await waiters.deleteRecord();
+    let days = [1, 2, 3];
+    await waiters.selectDay({ name: 'Apiwe', day: days });
+    await waiters.selectDay({ name: 'Mandla', day: days });
+    await waiters.selectDay({ name: 'Lwazi', day: days });
+    days = [1, 2, 5];
+    await waiters.selectDay({ name: 'Peter', day: days });
+    await waiters.selectDay({ name: 'Zee', day: days });
+    days = [3, 4, 5];
+    await waiters.selectDay({ name: 'Lee', day: days });
+    days = [2, 5, 6];
+    await waiters.selectDay({ name: 'Vusi', day: days });
+    days = [5, 6, 7];
+    await waiters.selectDay({ name: 'Rosanne', day: days });
 
-    })
-
-    it('Should check if days which days are selected by logged waiter',async () => {
-
-        await waiters.deleteRecord()
-        var days = [1, 2, 3]
-        await waiters.selectDay({ name: 'Apiwe', day: days })
-        await waiters.selectDay({ name: 'Mandla', day: days })
-        await waiters.selectDay({ name: 'Lwazi', day: days })
-        days = [1, 2, 5]
-        await waiters.selectDay({ name: 'Peter', day: days })
-        await waiters.selectDay({ name: 'Zee', day: days })
-        days[3, 4, 5]
-        await waiters.selectDay({ name: 'Lee', day: days })
-        days = [2, 5, 6]
-        await waiters.selectDay({ name: 'Vusi', day: days })
-        days = [5, 6, 7]
-        await waiters.selectDay({ name: 'Rosanne', day: days })
-
-        assert.deepEqual([
-            {
-              day: 'Sunday',
-              id: 1,
-              selected: true
-            },
-            {
-              day: 'Monday',
-              id: 2,
-              selected: true
-            },
-            {
-              day: 'Tuesday',
-              id: 3,
-              selected: true
-            },
-            {
-              day: 'Wednesday',
-              id: 4,
-              selected: false
-            },
-            {
-              day: 'Thursday',
-              id: 5,
-              selected: false
-            },
-            {
-              day: 'Friday',
-              id: 6,
-              selected: false
-            },
-            {
-              day: 'Saturday',
-              id: 7,
-              selected: false
-            }
-          ]
-          , await waiters.chechDays("Lwazi"))
-
-    })
-
-    describe('Colors update on admin view', () => {
-        it('Should return green for days that have three waiters, yellow for days short with waiters/ have less than three waiters and red for days have more than three waiters', async () => {
-            await waiters.deleteRecord()
-            var days = [1, 2, 3]
-            await waiters.selectDay({ name: 'Apiwe', day: days })
-            await waiters.selectDay({ name: 'Mandla', day: days })
-            await waiters.selectDay({ name: 'Lwazi', day: days })
-            days = [1, 2, 5]
-            await waiters.selectDay({ name: 'Peter', day: days })
-            await waiters.selectDay({ name: 'Zee', day: days })
-            days[3, 4, 5]
-            await waiters.selectDay({ name: 'Lee', day: days })
-            days = [2, 5, 6]
-            await waiters.selectDay({ name: 'Vusi', day: days })
-            days = [5, 6, 7]
-            await waiters.selectDay({ name: 'Rosanne', day: days })
-            assert.deepEqual([
-                {
-                    color: 'bg-danger',
-                    day: 'Sunday',
-                    id: 1
-                },
-                {
-                    color: 'bg-danger',
-                    day: 'Monday',
-                    id: 2
-                },
-                {
-                    color: 'bg-success',
-                    day: 'Tuesday',
-                    id: 3
-                },
-                {
-                    color: 'bg-warning',
-                    day: 'Wednesday',
-                    id: 4
-                },
-                {
-                    color: 'bg-danger',
-                    day: 'Thursday',
-                    id: 5
-                },
-                {
-                    color: 'bg-warning',
-                    day: 'Friday',
-                    id: 6
-                },
-                {
-                    color: 'bg-warning',
-                    day: 'Saturday',
-                    id: 7
-                }
-            ]
-                , await waiters.daysColor())
-        })
-
-    })
-
-})
+    assert.deepEqual([
+      {
+        day: 'Sunday',
+        id: 1,
+        selected: true,
+      },
+      {
+        day: 'Monday',
+        id: 2,
+        selected: true,
+      },
+      {
+        day: 'Tuesday',
+        id: 3,
+        selected: true,
+      },
+      {
+        day: 'Wednesday',
+        id: 4,
+        selected: false,
+      },
+      {
+        day: 'Thursday',
+        id: 5,
+        selected: false,
+      },
+      {
+        day: 'Friday',
+        id: 6,
+        selected: false,
+      },
+      {
+        day: 'Saturday',
+        id: 7,
+        selected: false,
+      },
+    ],
+    await waiters.chechDays('Lwazi'));
+  });
+  // eslint-disable-next-line no-undef
+  describe('Colors update on admin view', () => {
+    // eslint-disable-next-line no-undef
+    it('Should return green for days that have three waiters, yellow for days short with waiters/ have less than three waiters and red for days have more than three waiters', async () => {
+      await waiters.deleteRecord();
+      let days = [1, 2, 3];
+      await waiters.selectDay({ name: 'Apiwe', day: days });
+      await waiters.selectDay({ name: 'Mandla', day: days });
+      await waiters.selectDay({ name: 'Lwazi', day: days });
+      days = [1, 2, 5];
+      await waiters.selectDay({ name: 'Peter', day: days });
+      await waiters.selectDay({ name: 'Zee', day: days });
+      days = [3, 4, 5];
+      await waiters.selectDay({ name: 'Lee', day: days });
+      days = [2, 5, 6];
+      await waiters.selectDay({ name: 'Vusi', day: days });
+      days = [5, 6, 7];
+      await waiters.selectDay({ name: 'Rosanne', day: days });
+      assert.deepEqual([
+        {
+          color: 'bg-danger',
+          day: 'Sunday',
+          id: 1,
+        },
+        {
+          color: 'bg-danger',
+          day: 'Monday',
+          id: 2,
+        },
+        {
+          color: 'bg-success',
+          day: 'Tuesday',
+          id: 3,
+        },
+        {
+          color: 'bg-warning',
+          day: 'Wednesday',
+          id: 4,
+        },
+        {
+          color: 'bg-danger',
+          day: 'Thursday',
+          id: 5,
+        },
+        {
+          color: 'bg-warning',
+          day: 'Friday',
+          id: 6,
+        },
+        {
+          color: 'bg-warning',
+          day: 'Saturday',
+          id: 7,
+        },
+      ],
+      await waiters.daysColor());
+    });
+  });
+});
